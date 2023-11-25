8925ecb2c08a42cc67d85f897981527a9d4b75d0
Mabel <ortizgladys874@gmail.com>
2023-11-25 03:15:34 -0300

    DISCULPE LA ENTREGA TARDIA POR FAVOR PROFESOR

---
+++ js/print.window.js
@@ -0,0 +1,9 @@
+function imprimirWindow (objeto){
+    var objetoSeleccionado = document.getElementById(objeto);
+    var imprimirObjeto = window.open('','Impresion');
+    imprimirObjeto.document.write(objetoSeleccionado.innerHTML);
+    imprimirObjeto.document.close();
+    imprimirObjeto.print();
+    imprimirObjeto.close();
+
+}
